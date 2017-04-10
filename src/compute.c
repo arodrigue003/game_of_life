8c56e6c489a215cbc1994e45c428d142cdc0fb11
Adrien Rodrigues <arodrigue003@enseirb-matmeca.fr>
2017-04-10 17:01:32 +0200

    [ADD] iterative implementation v0

--- src/compute.c
+++ src/compute.c
@@ -5,6 +5,9 @@
 #include "ocl.h"
 
 #include <stdbool.h>
+#include <stdio.h>
+
+static unsigned couleur = 0xFFFF00FF; // Yellow
 
 unsigned version = 0;
 
@@ -17,31 +20,31 @@ unsigned compute_v2 (unsigned nb_iter);
 unsigned compute_v3 (unsigned nb_iter);
 
 void_func_t first_touch [] = {
-  NULL,
-  first_touch_v1,
-  first_touch_v2,
-  NULL,
+    NULL,
+    first_touch_v1,
+    first_touch_v2,
+    NULL,
 };
 
 int_func_t compute [] = {
-  compute_v0,
-  compute_v1,
-  compute_v2,
-  compute_v3,
+    compute_v0,
+    compute_v1,
+    compute_v2,
+    compute_v3,
 };
 
 char *version_name [] = {
-  "Séquentielle",
-  "OpenMP",
-  "OpenMP zone",
-  "OpenCL",
+    "Séquentielle",
+    "OpenMP",
+    "OpenMP zone",
+    "OpenCL",
 };
 
 unsigned opencl_used [] = {
-  0,
-  0,
-  0,
-  1,
+    0,
+    0,
+    0,
+    1,
 };
 
 ///////////////////////////// Version séquentielle simple
@@ -50,17 +53,38 @@ unsigned opencl_used [] = {
 unsigned compute_v0 (unsigned nb_iter)
 {
 
-  for (unsigned it = 1; it <= nb_iter; it ++) {
-    for (int i = 0; i < DIM; i++)
-      for (int j = 0; j < DIM; j++)
-	next_img (i, j) = cur_img (j, i);
-    
-    swap_images ();
-  }
-  // retourne le nombre d'étapes nécessaires à la
-  // stabilisation du calcul ou bien 0 si le calcul n'est pas
-  // stabilisé au bout des nb_iter itérations
-  return 0;
+    for (unsigned it = 1; it <= nb_iter; it ++) {
+        for (int i = 1; i < DIM-1; i++) {
+            for (int j = 1; j < DIM-1; j++) {
+                int n = (cur_img(i-1, j-1) !=0) +
+                        (cur_img(i-1, j)   !=0) +
+                        (cur_img(i-1, j+1) !=0) +
+                        (cur_img(i  , j-1) !=0) +
+                        (cur_img(i  , j+1) !=0) +
+                        (cur_img(i+1, j-1) !=0) +
+                        (cur_img(i+1, j)   !=0) +
+                        (cur_img(i+1, j+1) !=0);
+                if (cur_img(i, j)) {
+                    if (n>=2 && n<=3)
+                        next_img(i,j) = couleur;
+                    else
+                        next_img(i,j) = 0;
+                }
+                else {
+                    if (n==3)
+                        next_img(i,j) = couleur;
+                    else
+                        next_img(i,j) = 0;
+                }
+
+            }
+        }
+        swap_images ();
+    }
+    // retourne le nombre d'étapes nécessaires à la
+    // stabilisation du calcul ou bien 0 si le calcul n'est pas
+    // stabilisé au bout des nb_iter itérations
+    return 0;
 }
 
 
@@ -68,19 +92,19 @@ unsigned compute_v0 (unsigned nb_iter)
 
 void first_touch_v1 ()
 {
-  int i,j ;
+    int i,j ;
 
 #pragma omp parallel for
-  for(i=0; i<DIM ; i++) {
-    for(j=0; j < DIM ; j += 512)
-      next_img (i, j) = cur_img (i, j) = 0 ;
-  }
+    for(i=0; i<DIM ; i++) {
+        for(j=0; j < DIM ; j += 512)
+            next_img (i, j) = cur_img (i, j) = 0 ;
+    }
 }
 
 // Renvoie le nombre d'itérations effectuées avant stabilisation, ou 0
 unsigned compute_v1(unsigned nb_iter)
 {
-  return 0;
+    return 0;
 }
 
 
@@ -95,7 +119,7 @@ void first_touch_v2 ()
 // Renvoie le nombre d'itérations effectuées avant stabilisation, ou 0
 unsigned compute_v2(unsigned nb_iter)
 {
-  return 0; // on ne s'arrête jamais
+    return 0; // on ne s'arrête jamais
 }
 
 
@@ -104,5 +128,5 @@ unsigned compute_v2(unsigned nb_iter)
 // Renvoie le nombre d'itérations effectuées avant stabilisation, ou 0
 unsigned compute_v3 (unsigned nb_iter)
 {
-  return ocl_compute (nb_iter);
+    return ocl_compute (nb_iter);
 }
